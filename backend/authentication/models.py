906061c4ff15a1a5cee3c0e6f0b0bb74f6e744ed
olegTervo <olegtervo@icloud.com>
2024-10-21 11:27:53 +0300

    User id is GUID type

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
@@ -35,6 +36,8 @@ class CustomUserManager(BaseUserManager):
         return self.create_user(email, password, **extra_fields)
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -58,6 +58,7 @@ class UserProfileView(APIView):
     def get(self, request):
         user = request.user
         return Response({
+            'id': user.id,
             'first_name': user.first_name,
             'last_name': user.last_name,
             'email': user.email,
